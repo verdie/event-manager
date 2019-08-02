8db5afdd7161c8fa1fb97008e198dcd6af148df1
halynaburdiian <verdamtion@gmail.com>
2019-08-02 11:07:56 +0200

    Delete event

--- src/actions/events.js
+++ src/actions/events.js
@@ -2,6 +2,8 @@ import request from 'superagent'
 
 export const EVENTS_FETCHED = 'EVENTS_FETCHED'
 export const EVENT_CREATE_SUCCESS = 'EVENT_CREATE_SUCCESS'
+export const EVENT_FETCHED = 'EVENT_FETCHEDS'
+export const EVENT_DELETE_SUCCESS = 'EVENT_DELETE_SUCCESS'
 
 const baseUrl = 'http://localhost:4000'
 
@@ -28,17 +30,47 @@ export const loadEvents = () => (dispatch, getState) => {
 
 
 
-const eventCreateSuccess = event => ({
+const eventCreateSuccess = events => ({
   type: EVENT_CREATE_SUCCESS,
-  event
+  events
 })
 
 export const createEvent = (data) => dispatch => {
   request
-    .post(`${baseUrl}/events`)
+    .post(`${baseUrl}/event`)
     .send(data)
     .then(response => {
       dispatch(eventCreateSuccess(response.body))
     })
     .catch(console.error)
 }
+
+const loadEventSuccess = events => ({
+    type: EVENT_FETCHED,
+    events
+  })
+  
+  export const loadEvent = (id) => dispatch => {
+    request
+      .get(`${baseUrl}/event/${id}`)
+      .send()
+      .then(response => {
+        dispatch(loadEventSuccess(response.body))
+      })
+      .catch(console.error)
+  }
+
+  const deleteEventSuccess = events=>({
+    type: EVENT_DELETE_SUCCESS,
+    events
+  })
+
+  export const deleteEvent = (id) =>dispatch=>{
+    request
+      .delete(`${baseUrl}/event/${id}`)
+      .send()
+      .then(response =>{
+        dispatch(deleteEventSuccess(response.body.id))
+      })
+      .catch(console.error)
+  }
